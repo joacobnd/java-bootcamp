07c0eaffef884580dfc55b0a0ab8055764a1551e
joacobnd <joacomda@gmail.com>
2022-11-03 11:33:24 +0100

    Comparison Operators

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -3,35 +3,22 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
-        int number = 0;
-        int numberTwo = 0;
 
+        //Comparison Operators
+        // < <= > >= == !=
 
+        System.out.println(10 < 10);
+        System.out.println(10 <= 10);
+        System.out.println(10 > 10);
+        System.out.println(10 >= 10);
+        System.out.println(10 == 10);
+        System.out.println(10 != 10);
 
-//        Increment operator ++
-        System.out.println(number++);
-        System.out.println(number);
-
-        System.out.println();
-
-        System.out.println(++numberTwo);
-        System.out.println(numberTwo);
-
-
-//        Decrement operator --
-        System.out.println(number--);
-        System.out.println(number);
+        boolean isAdult = 18 >= 16;
 
         System.out.println();
-
-        System.out.println(--numberTwo);
-        System.out.println(numberTwo);
+        System.out.println(isAdult);
 
 
-//        int increment = number + 1;
-//        int decrement = number - 1;
-//
-//        System.out.println(increment);
-//        System.out.println(decrement);
     }
 }
