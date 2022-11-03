940f4fc2edc45b66454ed8ea3b1b44dbefbd4876
joacobnd <joacomda@gmail.com>
2022-11-03 11:18:49 +0100

    Arithmetic Operators

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -3,7 +3,15 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
-        int amount = 100_000_000;
-        System.out.println(amount);
+        int numberOne = 10;
+        int numberTwo = 3;
+        System.out.println(numberOne + numberTwo);
+        System.out.println(numberOne - numberTwo);
+        System.out.println(numberOne * numberTwo);
+        System.out.println(numberOne / numberTwo);
+        System.out.println(numberOne % numberTwo);
+
+        System.out.println((2 + 2) * (3 / 1) * 2);
+
     }
 }
