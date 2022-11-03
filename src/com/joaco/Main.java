566e60969fd82902e9508b343e2bc968420b4c56
joacobnd <joacomda@gmail.com>
2022-11-03 11:44:20 +0100

    Naming variables

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -3,20 +3,12 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
+        // Naming variable
+        int numberOfGoals = 0;
 
-      int numberOne = 2;
-      int numberTwo = 10;
-      numberTwo -= numberOne;
+        // booleans
 
-      System.out.println(numberTwo);
-
-      /*
-            x += y  is  x = x + y
-            x -= y  is  x = x - y
-            x *= y  is  x = x * y
-            x /= y  is  x = x / y
-            x %= y  is  x = x % y
-       */
-
+        boolean isAdult = true;
+        boolean hasCat = false;
     }
 }
