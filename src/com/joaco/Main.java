76a2b9c070c78c39c0c3e9822af40f8939e4f55b
joacobnd <joacomda@gmail.com>
2022-11-03 21:37:53 +0100

    Pass by Value with Reference Types

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -1,18 +1,22 @@
 package com.joaco;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        // Pass by Value with objects
+        Point pointA = new Point(10, 10);
+        Point pointB = pointA;
 
-        //Strings
-        String amigos = "Amigos";
-        String code = "code";
-        String brand = amigos + code;
-        System.out.println(brand.toUpperCase());
-        System.out.println(brand.toLowerCase());
-        System.out.println(brand.substring(0, 6).toUpperCase());
-        System.out.println("  ".isEmpty());
-        System.out.println("  ".isBlank());
-        System.out.println("  a   ".trim());
+        // En memory stack la referencia para ambos point es la misma, por ende la info que se guarda dentro del Heap es la misma para ambos(Comparten la misma info).
+
+        pointB.x = 100;
+        System.out.println(pointA);
+        System.out.println(pointB);
+
+        // al usar new point ahora si estamos creando en la memory stack un frame nuevo con una referencia en el heap(Ya no comparten la misma info).
+        pointB = new Point(55, 55);
     }
+
 }
