ece6a4ddde03703edad46314cdc33784f512d605
joacobnd <joacomda@gmail.com>
2022-11-03 10:56:42 +0100

    Comments

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -1,8 +1,15 @@
 package com.joaco;
 
 public class Main {
+
     public static void main(String[] args) {
-        // write your code here
+        // single line comment
+
+        /*
+            Multi
+            Line
+            Comment
+         */
         System.out.println("Hello Java!");
         System.out.println(10 + 10);
     }
