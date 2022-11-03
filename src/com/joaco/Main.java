3e711ae6dccf7ab46d782edb6c76f24c7e00695b
joacobnd <joacomda@gmail.com>
2022-11-03 11:40:27 +0100

    Shorthand re-assignment

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -4,21 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Comparison Operators
-        // < <= > >= == !=
+      int numberOne = 2;
+      int numberTwo = 10;
+      numberTwo -= numberOne;
 
-        System.out.println(10 < 10);
-        System.out.println(10 <= 10);
-        System.out.println(10 > 10);
-        System.out.println(10 >= 10);
-        System.out.println(10 == 10);
-        System.out.println(10 != 10);
+      System.out.println(numberTwo);
 
-        boolean isAdult = 18 >= 16;
-
-        System.out.println();
-        System.out.println(isAdult);
-
+      /*
+            x += y  is  x = x + y
+            x -= y  is  x = x - y
+            x *= y  is  x = x * y
+            x /= y  is  x = x / y
+            x %= y  is  x = x % y
+       */
 
     }
 }
