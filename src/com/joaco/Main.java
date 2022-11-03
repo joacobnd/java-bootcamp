ba9444e911556eeff1edc22c6bc79d5d1c57ee23
joacobnd <joacomda@gmail.com>
2022-11-03 11:15:03 +0100

    Numeric Literals with Underscore

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -3,13 +3,7 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
-       // primitives
-        int number1 = 20;
-        int number2 = 20;
-        int result = number1 + number2;
-        System.out.println(result);
-        double pi = 3.14;
-        boolean isAdult = false;
-        char a = 'A';
+        int amount = 100_000_000;
+        System.out.println(amount);
     }
 }
