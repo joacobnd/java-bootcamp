8f1a362b847bc92f125a6ac6416480d007732e53
joacobnd <joacomda@gmail.com>
2022-11-04 17:48:23 +0100

    Enhanced for loop

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-Z
-*src/com/joaco/exercises/ExercisesMain.java,8\4\840f6d65210993c2ac3af2aba217255b1306cedb
--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -8,14 +8,19 @@ public class Main {
         // Loops and Arrays
         String[] names = {"James", "Nadia", "Sophia", "Alex", "Saleh", "Aisha"};
 
+        System.out.println("for i loop:");
         for (int i = 0; i < names.length ; i++) {
             System.out.println(names[i]);
         }
 
+
         System.out.println("");
+        System.out.println("enhanced for loop:");
         for (String name : names) {
             System.out.println(name);
         }
+
+
 
 
 //        System.out.println(names[0]);
