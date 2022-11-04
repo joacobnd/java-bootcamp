b2d8288b04706dd4f4854924c4080b6465987a2b
joacobnd <joacomda@gmail.com>
2022-11-04 14:50:31 +0100

    For Loop

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -5,7 +5,18 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
+        // Loops
 
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println(i + " Hello!");
+        }
+
+        System.out.println("0");
+        //Reverse loop
+        for (int i = 10; i >= 0 ; i--) {
+            System.out.println(i + " Hello!");
+        }
 
     }
 
