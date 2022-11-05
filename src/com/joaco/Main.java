d4825c9928cb9cad71898b05b3d690810a861da1
joacobnd <joacomda@gmail.com>
2022-11-05 16:58:04 +0100

    || OR Logical Operator

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -5,21 +5,14 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
-        // && AND Logical Operators
+        // || OR Logical Operators
 
-        int age = 4;
-        boolean isAdult = age >= 18;
-
-        if (isAdult) {
-            System.out.println("is adult :)");
-        } else if (age == 0) {
-            System.out.println("baby :)");
-        } else if (age > 0 && age <=3) {                  //All boolean values need to be true if just one is false the code is skipped
-            System.out.println("toddler :)");
+        String gender = "FEMALe";
+        if (gender.equalsIgnoreCase("female") || gender.equalsIgnoreCase("male")) {   //one of the boolean could be true is no one is true then skip it
+            System.out.println("Valid Gender");
         } else {
-            System.out.println("is not adult :(");
+            System.out.println("Invalid gender");
         }
-
 
     }
 
