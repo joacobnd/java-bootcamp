b47d29c553a3a6eaf61b297d7ef1cb2a210f1f40
joacobnd <joacomda@gmail.com>
2022-11-05 17:12:02 +0100

    switch statement

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -5,13 +5,19 @@ package com.joaco;
 public class Main {
 
     public static void main(String[] args) {
-        // || OR Logical Operators
+        // switch
+        char grade = 'D';
 
-        String gender = "FEMALe";
-        if (gender.equalsIgnoreCase("female") || gender.equalsIgnoreCase("male")) {   //one of the boolean could be true is no one is true then skip it
-            System.out.println("Valid Gender");
-        } else {
-            System.out.println("Invalid gender");
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Pass!");
+                break;
+            default:
+                System.out.println("Fail!");
         }
 
     }
