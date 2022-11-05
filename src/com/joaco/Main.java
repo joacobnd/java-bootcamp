f7cd8ef28efb0e224ef8a11d271e26f297280d61
joacobnd <joacomda@gmail.com>
2022-11-05 17:18:27 +0100

    New switch statement

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -6,19 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         // switch
-        char grade = 'D';
+        char grade = 'A';
 
-        switch (grade) {
-            case 'A':
-                System.out.println("Excellent!");
-                break;
-            case 'B':
-            case 'C':
-                System.out.println("Pass!");
-                break;
-            default:
-                System.out.println("Fail!");
-        }
+//        switch (grade) {
+//            case 'A':
+//                System.out.println("Excellent!");
+//                break;
+//            case 'B':
+//            case 'C':
+//                System.out.println("Pass!");
+//                break;
+//            default:
+//                System.out.println("Fail!");
+//        }
+
+        String result = switch (grade) {
+            case 'A' -> "Excellent!";
+            case 'B' -> "Pass!";
+            case 'C' -> "Pass!";
+            default -> "Fail!";
+        };
+
+        System.out.println(result.toUpperCase());
+
 
     }
 
