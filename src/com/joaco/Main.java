d29bac8a58364724e38196d73d6d3493fbcbb352
joacobnd <joacomda@gmail.com>
2022-11-06 22:40:49 +0100

    Access Modifiers: static keyword

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -7,12 +7,16 @@ public class Main {
     public static String foo = "bar";                 // public: Could be used in all packages
     private static String apple = "apple";            // private: Can be used only in this class
     static String brand = "Amigoscode";               // default: Can be used only in this package
+    String name = "Joaquin";
 
     public static void main(String[] args) {
         // Access Modifiers - Access Level
         // classes, methods, attributes & constructors
         // default, public, private, protected
 
+        System.out.println(brand);
+        Main m = new Main();
+        System.out.println(m.name);
     }
 
 }
