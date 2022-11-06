8fa2873ec0169020e55680977e1d88066f9457ee
joacobnd <joacomda@gmail.com>
2022-11-06 19:39:11 +0100

    Packages and Import with fully qualified name

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -1,34 +1,14 @@
 package com.joaco;
 
 
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        // switch
-        char grade = 'A';
-
-//        switch (grade) {
-//            case 'A':
-//                System.out.println("Excellent!");
-//                break;
-//            case 'B':
-//            case 'C':
-//                System.out.println("Pass!");
-//                break;
-//            default:
-//                System.out.println("Fail!");
-//        }
-
-        String result = switch (grade) {
-            case 'A' -> "Excellent!";
-            case 'B' -> "Pass!";
-            case 'C' -> "Pass!";
-            default -> "Fail!";
-        };
-
-        System.out.println(result.toUpperCase());
-
+        // Packages
+        Date date = new Date();
+        java.sql.Date date1 = new java.sql.Date(1);
 
     }
 
---
+++ src/com/joaco/email/EmialValidator.java
@@ -0,0 +1,5 @@
+package com.joaco.email;
+
+public class EmialValidator {
+
+}
