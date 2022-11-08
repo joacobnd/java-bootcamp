56084fbe651dc360280247adcced0377a8680128
joacobnd <joacomda@gmail.com>
2022-11-08 14:19:39 +0100

    Continue keyword and Loops

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -4,14 +4,14 @@ package com.joaco;
 public class Main {
     public static void main(String[] args) {
         // Break and Continue
-        var numbers = new int[]{1, 2, 3, 4, 5, 6};
-        for (int number : numbers) {
-            if (number == 3) {
-                break;
+        var names = new String[]{"Ali", "Andre", "Jamila", "Jones", "Matt", "Ahmed"};
+        for (String name : names) {
+            if (name.startsWith("A")) {
+                continue;
             }
-            System.out.println(number);
+            System.out.println(name);
+            }
         }
-    }
 
 
 }
