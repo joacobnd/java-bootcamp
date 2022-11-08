f891619e207ab700f44169e4d82b04dba7bc8be0
joacobnd <joacomda@gmail.com>
2022-11-08 23:58:01 +0100

    Working with Strings

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -2,14 +2,19 @@ package com.joaco;
 
 public class Main {
 
-    enum TShirtSize{
-        S,
-        M,
-        L,
-        XL
-    }
 
     public static void main(String[] args) {
+        // Strings
+        String name = "Jamila";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println("   ".isBlank());
+        System.out.println("   ".isEmpty());
+        System.out.println(" Hellooo   ".trim());
+        System.out.println(name.startsWith("J"));
+        System.out.println(name.substring(3, 5));
+        System.out.println(name.replace("J", "Z"));
+        System.out.println(name.charAt(2));
 
     }
 
