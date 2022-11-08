6ac2a5a32b9ca46df570ae0f3eee91028da5b7ab
joacobnd <joacomda@gmail.com>
2022-11-09 00:19:41 +0100

    Useful String static Methods

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -5,16 +5,14 @@ public class Main {
 
     public static void main(String[] args) {
         // Strings
-        String name1 = "Jamila";
-        String name2 = "Jamila";
-        String name3 = new String("Jamila");
-        System.out.println(name1==name2);      // NUNCA USSAR == para comparar STRINGS
-        System.out.println(name1==name3);
-        System.out.println(name1.equals(name2));
-        System.out.println(name1.equals(name3));
 
-
-
+        String number = String.valueOf(1);
+        System.out.println(number);
+        String format = String.format("Number %s", number);
+        System.out.println(format);
+        String[] names = {"Jamila", "Alex", "Joaquin"};
+        String list = String.join(", ", names);     //Convertir un Array en String
+        System.out.println(list);
 
     }
 
