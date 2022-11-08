a5378460391793ebd83c484e7473bae4042f46f7
joacobnd <joacomda@gmail.com>
2022-11-08 14:23:20 +0100

    Return keyword and void methods

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -4,14 +4,19 @@ package com.joaco;
 public class Main {
     public static void main(String[] args) {
         // Break and Continue
-        var names = new String[]{"Ali", "Andre", "Jamila", "Jones", "Matt", "Ahmed"};
+        var names = new String[]{"Joaquin", "Ali", "Andre", "Jamila", "Jones", "Matt", "Ahmed"};
         for (String name : names) {
             if (name.startsWith("A")) {
-                continue;
+                return;
             }
             System.out.println(name);
-            }
+
+
         }
+
+        System.out.println("End of the main method");
+
+    }
 
 
 }
