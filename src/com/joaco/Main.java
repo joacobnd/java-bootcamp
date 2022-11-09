cfee8ed5e817652316b5ba902ffdd78ccdc5e344
joacobnd <joacomda@gmail.com>
2022-11-09 13:08:42 +0100

    Exploring BigDecimal Methods

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -2,22 +2,18 @@ package com.joaco;
 
 
 import java.math.BigDecimal;
-import java.time.*;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        //The Problem With double
-        double number1 = 0.02;
-        double number2 = 0.03;
-        double result = number2 - number1;
+        //Working with BigDecimal
+        BigDecimal number = new BigDecimal("10.35");
+        BigDecimal number2 = new BigDecimal("23.65");
+//        BigDecimal result = number.add(number2);
+        BigDecimal result = number2.subtract(number);
         System.out.println(result);
 
-        BigDecimal number3 = new BigDecimal("0.02");
-        BigDecimal number4 = new BigDecimal("0.03");
-        BigDecimal subResult = number4.subtract(number3);
-        System.out.println(subResult);
     }
 
 
