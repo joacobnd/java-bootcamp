190bd39ad7976adbbc33eec10493f03ff1f265c0
joacobnd <joacomda@gmail.com>
2022-11-09 18:07:08 +0100

    Using OR | within Catch

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -9,17 +9,16 @@ public class Main {
     public static void main(String[] args) {
         // Exceptions
         try {
-            int number = Integer.parseInt("1");
+            int number = Integer.parseInt("1x");
             System.out.println(number);
 
             for (int i = 10; i >= 0; i--) {
                 System.out.println(10 / i);
             }
 
-        } catch (NumberFormatException e) {
-            System.out.println("NumberFormatException: Failed to parse");
-        } catch (ArithmeticException e) {
-            System.out.println("ArithmeticException: Cannot divide by Zero");
+        } catch (NumberFormatException | ArithmeticException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();  //Print the StackTrace (Red message error)
         }
 
 
