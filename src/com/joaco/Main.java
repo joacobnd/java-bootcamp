ff9353db6aea95c3e2ee8590d4927bd39f4f6bd5
joacobnd <joacomda@gmail.com>
2022-11-09 12:36:32 +0100

    Dates: Other Date Classes

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -8,17 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         // Working With Dates
-        LocalDateTime nowSpain = LocalDateTime.now(ZoneId.of("Europe/Madrid"));
-        LocalDateTime nowArg = LocalDateTime.now(ZoneId.of("America/Argentina/Buenos_Aires"));
-
-        System.out.println(nowSpain);
-        System.out.println(nowArg);
-
-        ZoneId.getAvailableZoneIds();
-
-        for (String zones : ZoneId.getAvailableZoneIds()) {
-            System.out.println(zones);
-        }
+        System.out.println(LocalDateTime.now());
+        System.out.println(ZonedDateTime.now());
+        System.out.println(Instant.now());
 
 
 
