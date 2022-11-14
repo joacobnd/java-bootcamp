63a5649ccd9246f69c197695938f2e73c4d756f2
joacobnd <joacomda@gmail.com>
2022-11-14 15:58:33 +0100

    Utility Classes

--- src/com/joaco/person/PersonService.java
+++ src/com/joaco/person/PersonService.java
@@ -1,9 +1,13 @@
 package com.joaco.person;
 
+import com.joaco.utils.StringUtils;
+
+import static com.joaco.utils.StringUtils.isEmpty;
+
 public class PersonService {
 
     public int addPerson(Person person) {
-        if (person.getFirstName().isBlank()) {
+        if (StringUtils.isEmpty(person.getFirstName())) {
             throw new IllegalArgumentException("First name cannot be null or empty");
         }
         // store person to db
---
+++ src/com/joaco/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package com.joaco.utils;
+
+public class StringUtils {
+    public static boolean isEmpty(String s) {
+        // this pointless but mainly for example
+        return s.isEmpty();
+    }
+}
