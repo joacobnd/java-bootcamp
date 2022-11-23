1eb511d4ba93c7eb30160b9e1b69c46efa0df186
joacobnd <joacomda@gmail.com>
2022-11-23 18:55:01 +0100

    Arrays: working with arrays

---
+++ src/com/joaco/TheArrays.java
@@ -0,0 +1,36 @@
+package com.joaco;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+        colors[2] = "yellow";
+        System.out.println("\nUsing Arrays.toString():");
+        System.out.println(Arrays.toString(colors));
+        System.out.println("\nUsing sout(color[1]) index by index:");
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+
+        System.out.println("\nUsing for:");
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        System.out.println("\nUsing for invert");
+        for (int i = colors.length - 1; i >= 0; i--) {
+            System.out.println(colors[i]);
+        }
+
+        System.out.println("\nUsing Arrays.stream(colors).forEach(System.out::println)");
+        Arrays.stream(colors).forEach(System.out::println);
+
+
+    }
+}
--- src/com/joaco/solid/Main.java
+++ src/com/joaco/solid/Main.java
@@ -1,24 +1,10 @@
 package com.joaco.solid;
 
 
-import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
-        String brand = "Joaquin";
-        Optional<String> brandOptional = Optional.ofNullable(brand);
-        String ifBrandEmpty = brandOptional.orElse("The brand is empty");
 
-        brandOptional.ifPresentOrElse(s -> System.out.println(s.toUpperCase()), () -> System.out.println(ifBrandEmpty));
-
-        goUpperCase(Optional.empty());
-
-
-
-    }
-
-    public static void goUpperCase(Optional<String> input) {
-        input.ifPresent(s -> System.out.println(s.toUpperCase()));
     }
 }
