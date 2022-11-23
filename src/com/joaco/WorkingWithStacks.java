6a6dddf2e436584908d2737d2326a137b81adf68
joacobnd <joacomda@gmail.com>
2022-11-23 22:31:04 +0100

    Arrays: working with Stacks

---
+++ src/com/joaco/WorkingWithStacks.java
@@ -0,0 +1,18 @@
+package com.joaco;
+
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1); //agregamos elemento
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.peek()); //muestra el ultimo elemento
+        System.out.println(stack.size()); //muestra el tamaño
+        System.out.println(stack.pop()); //muestra y elimina el ultimo elemento
+        System.out.println(stack.size());
+        System.out.println(stack.isEmpty());
+    }
+}
