dc5271948808bb36e016f48f66051aa56e3c1c12
joacobnd <joacomda@gmail.com>
2022-11-23 18:14:34 +0100

    Optionals: testing ways to use optionals

--- src/com/joaco/solid/Main.java
+++ src/com/joaco/solid/Main.java
@@ -1,13 +1,24 @@
 package com.joaco.solid;
 
 
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-        String brand = null;
-        if (brand == null) {
-            System.out.println("brand is null");
-        } else System.out.println(brand.toUpperCase());
+        String brand = "Joaquin";
+        Optional<String> brandOptional = Optional.ofNullable(brand);
+        String ifBrandEmpty = brandOptional.orElse("The brand is empty");
 
+        brandOptional.ifPresentOrElse(s -> System.out.println(s.toUpperCase()), () -> System.out.println(ifBrandEmpty));
+
+        goUpperCase(Optional.empty());
+
+
+
+    }
+
+    public static void goUpperCase(Optional<String> input) {
+        input.ifPresent(s -> System.out.println(s.toUpperCase()));
     }
 }
