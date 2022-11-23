d7fce00b1c1fc139327248918a380fe19f47d2eb
joacobnd <joacomda@gmail.com>
2022-11-23 23:22:17 +0100

    Arrays: working with LinkedList

--- src/com/joaco/WorkingWithQueues.java
+++ src/com/joaco/WorkingWithQueues.java
@@ -1,11 +1,28 @@
 package com.joaco;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Joaquin", 20));
+        linkedList.add(new Person("Alexa", 30));
+        linkedList.addFirst(new Person("Maxi", 35));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+        System.out.println();
+        while (personListIterator.hasPrevious()) {
+            System.out.println(personListIterator.previous());
+        }
+    }
+
+    private static void queues() {
         Queue<Person> supermarketqueue = new LinkedList<>();
         supermarketqueue.add(new Person("Joaquin", 20));
         supermarketqueue.add(new Person("Alex", 21));
