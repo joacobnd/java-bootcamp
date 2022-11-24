807200349bdf7fa6b51a05fdf11fa68c06f41815
joacobnd <joacomda@gmail.com>
2022-11-24 23:57:37 +0100

    Generics: Upper Unbounded Wildcars

--- src/com/joaco/UpperBoundedWildcards.java
+++ src/com/joaco/UpperBoundedWildcards.java
@@ -6,19 +6,20 @@ import java.util.List;
 public class UpperBoundedWildcards {
     public static void main(String[] args) {
         // ? Wildcards
-        // Unbounded Wildcards
-        List<Object> list = Arrays.asList(1, 2, 3);
+        // Upper bounded Wildcards
+        List<Double> list = Arrays.asList(1.9, 2.3, 3.4);
         List<Integer> list2 = Arrays.asList(1, 2, 3);
-        List<String> list3 = Arrays.asList("1", "2", "3");
-        print(list);
+        List<Number> list3 = Arrays.asList(20, 17, 39);
+        printNumbers(list);
         System.out.println();
-        print(list2);
+        printNumbers(list2);
         System.out.println();
-        print(list3);
+        printNumbers(list3);
 
     }
 
-    static void print(List<?> list) {
+    //al especificar que extiende de Numbers podemos utilizar cualquier clase hija que tenga
+    static void printNumbers(List<? extends Number> list) {
         list.forEach(e -> {
             System.out.println(e.getClass().getName());
             System.out.println(e);
