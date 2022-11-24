50ffde54e0603fb9a382a8a58e8015d01c98b1d0
joacobnd <joacomda@gmail.com>
2022-11-24 20:33:44 +0100

    Generics: Naming Conventions

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -17,9 +17,19 @@ public class Main {
         List<Integer> numbers = new ArrayList<>();
         numbers.add(1);
 
+        //Map<K, V> K,V = Parameters
         Map<String, String> map = new HashMap<>();
 
         Map<Point, Double> map2 = new HashMap<>();
+
+        /*
+            Naming Convention
+            E - Element (used extensively by the Java) (Used in List here)
+            K - Key (Used in Map)
+            N - Number
+            T - Type
+            V - Value (Used in Map
+         */
     }
 
 
