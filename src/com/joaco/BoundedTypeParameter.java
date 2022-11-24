38ca942a414ac5aef82c6045d41078e9dd0c5a04
joacobnd <joacomda@gmail.com>
2022-11-24 23:33:23 +0100

    Generics: Bounded Type Parameters

---
+++ src/com/joaco/BoundedTypeParameter.java
@@ -0,0 +1,21 @@
+package com.joaco;
+
+public class BoundedTypeParameter {
+    public static void main(String[] args) {
+        Integer[] numbers = {30, 20, 10, 2, 8, 33};
+        Double[] numbers2 = {20.0, 10.0, 2.0};
+        System.out.println(countGreaterThan(numbers, 20));
+        System.out.println(countGreaterThan(numbers2, 15.5));
+
+    }
+
+    public static <T extends  Comparable<T>> int countGreaterThan(T[] numbers, T number) {
+        int count = 0;
+        for (T n : numbers) {
+            if (n.compareTo(number) > 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
