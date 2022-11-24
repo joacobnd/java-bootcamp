3eb4696ee84faa169f3e6373e0204b15ef8bff11
joacobnd <joacomda@gmail.com>
2022-11-24 13:38:58 +0100

    Arrays: working hashCode and HashFunction

--- src/com/joaco/WorkingWithMaps.java
+++ src/com/joaco/WorkingWithMaps.java
@@ -3,9 +3,20 @@ package com.joaco;
 import javax.sound.midi.Soundbank;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WorkingWithMaps {
     public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Jamila"), new Diamond("African Diamond"));
+
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(new Person("Jamila").hashCode());
+        System.out.println(map.get(new Person("Jamila")));
+
+    }
+
+    private static void maps() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Joaquin"));
         map.put(2, new Person("Ali"));
@@ -29,7 +40,36 @@ public class WorkingWithMaps {
         System.out.println(map.values());
     }
 
-    record Person(String name) {
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
     }
+
+    record Diamond(String name) {
+    }
+
 
 }
