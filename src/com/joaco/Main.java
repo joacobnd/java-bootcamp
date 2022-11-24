308b3394c83ac839ba8acc34ae9355ee8cc8bd54
joacobnd <joacomda@gmail.com>
2022-11-24 20:23:13 +0100

    Generics: ClassCastException Example2

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -7,15 +7,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List numbers = new ArrayList();
-        numbers.add(1);
-        numbers.add("2");
-        for (Object number : numbers) {
-            System.out.println((String)number);
-        }
+        Comparable number = 10;
+        number.compareTo("10"); //trying to compare with a String
 
-//        Exception in thread "main" java.lang.ClassCastException: class java.lang.Integer cannot be cast to class java.lang.String (java.lang.Integer and java.lang.String are in module java.base of loader 'bootstrap')
-//        at com.joaco.Main.main(Main.java:14)
+
+//        Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
+//        at java.base/java.lang.Integer.compareTo(Integer.java:71)
+//        at com.joaco.Main.main(Main.java:11)
+
 
 
     }
