5e338c6101783c627b64447a9f62630cbc192353
joacobnd <joacomda@gmail.com>
2022-11-24 20:30:17 +0100

    Generics: Type parameter and type arguments

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -1,22 +1,25 @@
 package com.joaco;
 
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
-        Comparable number = 10;
-        number.compareTo("10"); //trying to compare with a String
+        //List<E> E = Parameter
 
 
-//        Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
-//        at java.base/java.lang.Integer.compareTo(Integer.java:71)
-//        at com.joaco.Main.main(Main.java:11)
+        List<String> names = new ArrayList<>();  //String = Argumentos que le estamos pasando
+        names.add("1");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
 
+        Map<String, String> map = new HashMap<>();
 
-
+        Map<Point, Double> map2 = new HashMap<>();
     }
 
 
