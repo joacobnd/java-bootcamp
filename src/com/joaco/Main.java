fe95f316355482b3192e8b9689567dc67035deef
joacobnd <joacomda@gmail.com>
2022-11-24 20:20:41 +0100

    Generics: ClassCastException Example

--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -1,14 +1,22 @@
 package com.joaco;
 
 
-import com.joaco.car.CarDAO;
-import com.joaco.car.CarService;
-import com.joaco.email.EmailService;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        //TODO
+        List numbers = new ArrayList();
+        numbers.add(1);
+        numbers.add("2");
+        for (Object number : numbers) {
+            System.out.println((String)number);
+        }
+
+//        Exception in thread "main" java.lang.ClassCastException: class java.lang.Integer cannot be cast to class java.lang.String (java.lang.Integer and java.lang.String are in module java.base of loader 'bootstrap')
+//        at com.joaco.Main.main(Main.java:14)
+
 
     }
 
