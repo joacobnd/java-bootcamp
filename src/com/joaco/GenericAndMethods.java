afe55a3373ec877b57f4c75a4a670ba8e9fbdbd2
joacobnd <joacomda@gmail.com>
2022-11-24 23:20:01 +0100

    Generics: Generic and Methods

---
+++ src/com/joaco/GenericAndMethods.java
@@ -0,0 +1,29 @@
+package com.joaco;
+
+
+public class GenericAndMethods {
+    public static void main(String[] args) {
+
+
+        String[] names = {"Joaquin", "Maxi"};
+        Character[] letters = {'A', 'B', 'C'};
+        Integer[] numbers = {1, 2, 3};
+        //No podemos pasarle primitivos
+//        char[] letters = {'A', 'B', 'C'};
+
+        print(numbers);
+        System.out.println();
+        print(names);
+        System.out.println();
+        print(letters);
+
+
+    }
+
+
+    static <T> void print(T[] array) {
+        for (T e : array) {
+            System.out.println(e.getClass().getName() + " - " + e);
+        }
+    }
+}
--- src/com/joaco/Main.java
+++ src/com/joaco/Main.java
@@ -4,14 +4,6 @@ package com.joaco;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box<>();
-        box.set(new Phone(""));
-        System.out.println(box.get());
-
-        Box box2 = new Box<>();
-        box.set(new Letter("Joaquin"));
-        System.out.println(box.get());
-
 
     }
 
