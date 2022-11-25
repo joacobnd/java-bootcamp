954f0556b213a1ecba60b4b6163095c8d7884cba
joacobnd <joacomda@gmail.com>
2022-11-25 17:57:58 +0100

    Maven: Using Java Faker dependency

--- src/main/java/com/joaco/Main.java
+++ src/main/java/com/joaco/Main.java
@@ -1,7 +1,12 @@
 package com.joaco;
+
+import com.github.javafaker.Faker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+        Faker faker = new Faker();
+        System.out.println(faker.name().fullName());
+        System.out.println(faker.address().city());
     }
 }
 
