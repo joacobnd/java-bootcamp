5a65b0866b8404c951d1b8d7a329931ba79cce80
joacobnd <joacomda@gmail.com>
2022-11-26 12:08:07 +0100

    Spring Boot: RestController

--- src/main/java/com/joaco/person/PersonController.java
+++ src/main/java/com/joaco/person/PersonController.java
@@ -1,10 +1,15 @@
 package com.joaco.person;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 //API Layer
+@RestController
+@RequestMapping("/person")
 public class PersonController {
     private final PersonService personService;
 
@@ -13,6 +18,7 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @GetMapping
     public List<Person> fetchAllPeopple() {
         return personService.getPeopple();
     }
